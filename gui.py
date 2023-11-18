0e541d623cd059c2cf734709319515dd0cbd403b
Caio Daniel <127650414+Caio2a7@users.noreply.github.com>
2023-11-17 22:43:51 -0300

    Add files via upload

--- gui.py
+++ gui.py
@@ -1,6 +1,13 @@
 import customtkinter as ctk
 import tkinter as tk
 from PIL import Image, ImageTk
+import time
+import threading
+import csv
+
+day_list = ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado', 'Domingo']
+task_list = ['FMC', 'Cálculo', 'ITP', 'Currículo', 'Cursos', 'Leetcode', 'Leitura']
+time_list = [f'00:00:00', '0:00:00', '00:00:00', '00:00:00', '00:00:00', '00:00:00', '00:00:00']
 
 def expand():
     global sidebar_width
@@ -21,6 +28,42 @@ def expand():
             root.update()
             root.after(50)
 
+def save_time(dia, tarefa, horas, minutos, segundos):
+    global dias
+    dias[dia].configure(text=f"{horas}:{minutos}:{segundos}")
+
+def set_timer(dia, tarefa):
+    print(f"{dia} {tarefa}")
+    global is_running, j, i
+    if not is_running:
+        start_timer()
+    else:
+        stop_timer(dia, tarefa)
+
+def start_timer():
+    global timer_thread, is_running
+    is_running = True
+    timer_thread = threading.Thread(target=update_timer)
+    timer_thread.daemon = True
+    timer_thread.start()
+
+def stop_timer(dia, tarefa):
+    global tracking_time, is_running
+    horas = int(tracking_time // 3600)
+    minutos = (int(tracking_time % 3600)) // 60
+    segundos = (int(tracking_time % 60))
+    save_time(dia, tarefa, horas, minutos, segundos)
+    is_running = False
+    tracking_time = 0
+    print(f'{horas} horas | {minutos} minutos | {segundos} segundos')
+
+def update_timer():
+    global tracking_time
+    start_time = time.time()
+    while is_running:
+        tracking_time = time.time() - start_time
+        time.sleep(1)
+
 
 root = ctk.CTk()
 ctk.set_appearance_mode("dark")
@@ -38,51 +81,61 @@ root.iconbitmap(caminho_icone)
 sidebar_width = 100
 padx = 0
 
-main_frame = ctk.CTkScrollableFrame(master=root)
-main_frame.pack(fill='both', expand=True)
-
-left_sidebar_frame = ctk.CTkFrame(master=main_frame, width=sidebar_width, height=500, fg_color='#373838')
+left_sidebar_frame = ctk.CTkFrame(master=root, width=sidebar_width, height=500, fg_color='#373838')
 left_sidebar_frame.pack(side='left', fill='y')
 
 button_expand = ctk.CTkButton(master=left_sidebar_frame, text="Open", command=expand, width=75, fg_color='#00DF24')
 button_expand.place(x=10, y=10)
 
-right_sidebar_frame = ctk.CTkFrame(master=main_frame, width=sidebar_width, height=500, fg_color='#373838')
+right_sidebar_frame = ctk.CTkFrame(master=root, width=sidebar_width, height=500, fg_color='#373838')
 right_sidebar_frame.pack(side='right', fill='y')
 
 click_btn = Image.open('D:\Python_Projetos\Productivity_Analyser-main\Group 5.png')
 click_btn = click_btn.resize((30, 30))
 click_btn = ImageTk.PhotoImage(click_btn)
 
-day_list = ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado', 'Domingo']
-task_list = ['FMC', 'Cálculo', 'ITP', 'Currículo', 'Cursos', 'Leetcode', 'Leitura']
+content_frame = ctk.CTkScrollableFrame(master=root, width=800, height=1000)
+content_frame.pack(padx=5, pady=5)
+content_frame.lower()
 
-day_frame = ctk.CTkFrame(master=main_frame, height=300, width=1000, fg_color='#373838')
-day_frame.lower()
-day_frame.pack(padx=10, pady=10)
-text = ctk.CTkLabel(master=day_frame, text="Segunda", font=('Calibri', 15, 'bold'),
-                    padx=5, corner_radius=50,fg_color='green')
-text.place(x=5, y=0)
+tracking_time = 0
+is_running = False
+timer_thread = None
+lista_tempos = []
+class Create:
 
-add = 0
-for i in range(0, 5):
-    task_frame = ctk.CTkFrame(master=day_frame, height=35, width=800, fg_color='#110F13')
-    task_frame.place(y=60+add, x=420, anchor='center')
+    def __init(self):
+        pass
 
-    task_name = ctk.CTkLabel(master=task_frame, text='FMC', padx=5, font=('Calibri', 15, 'bold'))
-    task_name.place(x=20, y=2.1)
+    def create_day(self, day_index):
+        self.day_frame = ctk.CTkFrame(master=content_frame, height=300, width=1000, fg_color='#373838')
+        self.day_frame.pack(padx=10, pady=10)
+        text = ctk.CTkLabel(master=self.day_frame, text=f"{day_list[day_index]}", font=('Calibri', 15, 'bold'),
+                            padx=5, corner_radius=50, fg_color='green')
+        text.place(x=5, y=0)
+        self.add = 0
 
-    button_timer = tk.Button(master=task_frame, image=click_btn, borderwidth=0,
-                       bg="#110F13", activebackground='#110F13',
-                       border=0)
-    button_timer.place(x=100, y=1)
+    def create_task(self, day_index, task_index):
+        task_frame = ctk.CTkFrame(master=self.day_frame, height=35, width=760, fg_color='#110F13')
+        task_frame.place(y=60 + self.add, x=390, anchor='center')
+        task_name = ctk.CTkLabel(master=task_frame, text=f'{task_list[j]}', padx=5, font=('Calibri', 15, 'bold'))
+        task_name.place(x=20, y=2.1)
+        button_timer = tk.Button(master=task_frame, image=click_btn, borderwidth=0,
+                                 bg="#110F13", activebackground='#110F13',
+                                 border=0, command=lambda: set_timer(day_index, task_index))
+        button_timer.place(x=100, y=1)
+        tracked_time = ctk.CTkLabel(master=task_frame, text=f'{time_list[j]}', padx=5, font=('Calibri', 15, 'bold'))
+        tracked_time.place(x=135, y=2.1)
+        lista_tempos.append(tracked_time)
+        # x=40 e y=5
+        self.add += 50
 
-    tracked_time = ctk.CTkLabel(master=task_frame, text='6:10:00', padx=5, font=('Calibri', 15, 'bold'))
-    tracked_time.place(x=135, y=2.1)
-    #x=40 e y=5
-    add += 50
 
-"""
-"""
+habitus = Create()
+for i in range(0, 7):
+    habitus.create_day(i)
+    for j in range(0, 5):
+        habitus.create_task(i, j)
+
 
 root.mainloop()
